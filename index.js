3da6e2600c95b118953f1474fef31ff69323f37a
Glebson <glebsonsalmeida@gmail.com>
2025-09-15 19:58:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -74,13 +74,13 @@ app.post("/login", async (req, res) => {
     }
 });
 
-app.get("/teste"), async (req, res) => {
+app.get("/teste" , async (req, res) => {
     try{
-        return res.staus(200).json({ msg: "Servidor rodando!" })
+        return res.status(200).json({ msg: "Servidor rodando!" })
     }catch (erro) {
-        return res.staus(400).json({ erro: "Houve um problema com o servidor" })
+        return res.status(400).json({ erro: "Houve um problema com o servidor" })
     }
-} 
+})
 
 
 // --- FUNÇÕES DE BANCO DE DADOS ---
